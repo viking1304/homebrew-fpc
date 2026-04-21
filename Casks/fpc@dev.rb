f767a4439ce57ab7eefef94e7863d64304389953
Aleksandar Milanovic <viking1304@gmail.com>
2026-04-21 14:05:24 +0200

    Initial commit

---
+++ Casks/fpc@dev.rb
@@ -0,0 +1,22 @@
+cask "fpc@dev" do
+  arch arm: "aarch64", intel: "x86_64"
+  fpc_compiler = on_arch_conditional arm: "ppca64", intel: "ppcx64"
+
+  version "3.3.1,20260419"
+  sha256 arm:   "5665b813208bd29dba4e6014ca8d83b8d708ba9a13cb6f21eb028c013e220633",
+         intel: "ae8c692e879bb06905da4c37d94fdfa3243e0e870805ed08b9b44662264726af"
+
+  url "https://github.com/viking1304/fpc-trunk/releases/download/v#{version.csv.first}-#{version.csv.second}/fpc-#{version.csv.first}-#{version.csv.second}-#{arch}-macosx.dmg"
+  name "Free Pascal Compiler (trunk)"
+  desc "Unofficial nightly binaries for FPC trunk"
+  homepage "https://github.com/viking1304/fpc-trunk"
+
+  pkg "fpc-#{version.csv.first}-#{version.csv.second}-#{arch}-macosx.pkg"
+  binary "/usr/local/lib/fpc/#{version.csv.first}/#{fpc_compiler}"
+
+  uninstall pkgutil: "org.freepascal.fpc.trunk.#{arch}"
+
+  caveats do
+    files_in_usr_local
+  end
+end
